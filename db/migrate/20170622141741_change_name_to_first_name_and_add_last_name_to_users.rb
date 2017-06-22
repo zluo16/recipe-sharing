ab6f35dfc391af0710efed9efd63cd9f08e03a27
Garren Gotthardt <garren.x.gotthardt@gmail.com>
2017-06-22 10:20:17 -0400

    fixed first_name / last_name migration

--- db/migrate/20170622141741_change_name_to_first_name_and_add_last_name_to_users.rb
+++ db/migrate/20170622141741_change_name_to_first_name_and_add_last_name_to_users.rb
@@ -1,9 +1,6 @@
-class RenameNameColumnToFirstNameAndAddLastNameColumnOnUser < ActiveRecord::Migration[5.1]
+class ChangeNameToFirstNameAndAddLastNameToUsers < ActiveRecord::Migration[5.1]
   def change
     rename_column :users, :name, :first_name
-  end
-
-  def change
     add_column :users, :last_name, :string
   end
 end
