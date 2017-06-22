dced89ffed394ef37434ec3c768dbf5d2e05bbab
Garren Gotthardt <garren.x.gotthardt@gmail.com>
2017-06-22 10:53:23 -0400

    added publish feature to recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -44,7 +44,7 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:image, :author_id, :name, :prep_time, :cook_time, :instructions, :source, :vegetarian, :vegan, :gluten_free, :halal, :low_carb, :kosher)
+    params.require(:recipe).permit(:image, :author_id, :name, :prep_time, :cook_time, :instructions, :source, :vegetarian, :vegan, :gluten_free, :halal, :low_carb, :kosher, :published)
   end
 
 end
