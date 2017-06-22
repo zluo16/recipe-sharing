11e3a5bc49b10365ce9834bfaf90a7e229cb75aa
github username <github email address>
2017-06-22 16:29:10 -0400

    formatting and pictures

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,14 @@ class User < ApplicationRecord
   has_many :saver_recipes, foreign_key: "saver_id", class_name: "SaverRecipe"
   has_many :saved_recipes, through: :saver_recipes
   has_many :recipes, foreign_key: "author_id", class_name: "Recipe"
-  has_attached_file :image, styles: { medium: '300x300>', thumb: '100x100>' }, default_url: "/images/ND6AyZl.jpg"
+  has_attached_file :image, styles: { medium: '300x300>', thumb: '100x100>' }, default_url: :link_to_image
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   has_secure_password
   validates :email, uniqueness: true
+
+  def link_to_image
+    "https://lh3.googleusercontent.com/H9yAIsZYqbIOh_E1ON90chVhO6SYSD6ucV-XirZXkMFDqLRjGoztobaxx1XS9CB4lfg=w300"
+  end
+
 
 end
