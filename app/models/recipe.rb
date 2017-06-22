21e30d014de6c988342f7afa993243a45fff1b6d
zluo16 <zluo@alumni.berklee.edu>
2017-06-22 15:09:47 -0400

    minor change to recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,7 +4,7 @@ class Recipe < ApplicationRecord
   belongs_to :author, :class_name => "User", :foreign_key => :author_id
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
-  has_attached_file :image, styles: { medium: '300x300>', thumb: '100x100>' }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: '300x300>', thumb: '50x50>' }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   validates :name, presence: true
@@ -18,37 +18,37 @@ class Recipe < ApplicationRecord
 
   def self.featured
     @weekly_recipes = Recipe.where(:created_at => 7.days.ago..Time.now).where(published: true)
-    @weekly_recipes.sort_by{|r| r.savers.length}.reverse.first(10)
+    @weekly_recipes.sort_by{|r| r.savers.length}.reverse.first(5)
   end
 
   def self.top_users
     Recipe.published.sort_by{|r| r.savers.length}.reverse.map do |r|
       User.find(r.author_id)
-    end.uniq.first(10)
+    end.uniq.first(5)
   end
 
   def self.top_recipes
-    Recipe.published.sort_by{|r| r.savers.length}.reverse.first(10)
+    Recipe.published.sort_by{|r| r.savers.length}.reverse.first(5)
   end
 
   def self.fast_food
-    Recipe.published.sort_by{|r| r.prep_time + r.cook_time}.first(10)
+    Recipe.published.sort_by{|r| r.prep_time + r.cook_time}.first(5)
   end
 
   def self.top_vegan
-    Recipe.published.find_all{|r| r.vegan}.sort_by{|r| r.savers.length}.reverse.first(10)
+    Recipe.published.find_all{|r| r.vegan}.sort_by{|r| r.savers.length}.reverse.first(5)
   end
 
   def self.top_low_carb
-    Recipe.published.find_all{|r| r.low_carb}.sort_by{|r| r.savers.length}.reverse.first(10)
+    Recipe.published.find_all{|r| r.low_carb}.sort_by{|r| r.savers.length}.reverse.first(5)
   end
 
   def self.top_kosher
-    Recipe.published.find_all{|r| r.kosher}.sort_by{|r| r.savers.length}.reverse.first(10)
+    Recipe.published.find_all{|r| r.kosher}.sort_by{|r| r.savers.length}.reverse.first(5)
   end
 
   def self.top_halal
-    Recipe.published.find_all{|r| r.halal}.sort_by{|r| r.savers.length}.reverse.first(10)
+    Recipe.published.find_all{|r| r.halal}.sort_by{|r| r.savers.length}.reverse.first(5)
   end
 
 end
